cf9050bd72e868d29b038e84ec42f48d01d98b3f
Eugene Yamenko <yamenko@me.com>
2020-12-09 10:57:09 +1100

    Add MessagePack processor

---
+++ src/DotnetSerializationCompressionBenchmark/Processors/MessagePackProcessor.cs
@@ -0,0 +1,18 @@
+﻿using DotnetSerializationCompressionBenchmark.Contracts;
+using MessagePack;
+
+namespace DotnetSerializationCompressionBenchmark.Processors
+{
+    public class MessagePackProcessor : IProcessor
+    {
+        public byte[] Process<T>(T item)
+        {
+            return MessagePackSerializer.Serialize(item);
+        }
+
+        public T Unprocess<T>(byte[] bytes)
+        {
+            return MessagePackSerializer.Deserialize<T>(bytes);
+        }
+    }
+}
