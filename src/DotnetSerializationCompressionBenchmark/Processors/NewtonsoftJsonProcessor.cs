eb191c2fb264ce9f5dce209bb730e3076c1f928b
Eugene Yamenko <yamenko@me.com>
2020-12-08 16:26:29 +1100

    Add Newtonsoft.Json processor

---
+++ src/DotnetSerializationCompressionBenchmark/Processors/NewtonsoftJsonProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using DotnetSerializationCompressionBenchmark.Contracts;
+using Newtonsoft.Json;
+
+namespace DotnetSerializationCompressionBenchmark.Processors
+{
+    public class NewtonsoftJsonProcessor : IProcessor
+    {
+        public byte[] Process<T>(T item)
+        {
+            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(item));
+        }
+
+        public T Unprocess<T>(byte[] bytes)
+        {
+            return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(bytes));
+        }
+    }
+}
