233376ca1c2f21c4401a37b442bd3d902bcd7fba
Eugene Yamenko <yamenko@me.com>
2020-12-08 16:36:26 +1100

    Add MsgPack.Cli processor

---
+++ src/DotnetSerializationCompressionBenchmark/Processors/MsgPackCliProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using DotnetSerializationCompressionBenchmark.Contracts;
+using MsgPack.Serialization;
+
+namespace DotnetSerializationCompressionBenchmark.Processors
+{
+    public class MsgPackCliProcessor : IProcessor
+    {
+        public byte[] Process<T>(T item)
+        {
+            var serializer = MessagePackSerializer.Get<T>();
+
+            using var ms = new MemoryStream();
+
+            serializer.Pack(ms, item);
+
+            return ms.ToArray();
+        }
+
+        public T Unprocess<T>(byte[] bytes)
+        {
+            var serializer = MessagePackSerializer.Get<T>();
+
+            using var ms = new MemoryStream();
+
+            return serializer.Unpack(ms);
+        }
+    }
+}
