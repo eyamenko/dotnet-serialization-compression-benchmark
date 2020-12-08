73d9a504e15de27e3e2760eed60749784ec514b0
Eugene Yamenko <yamenko@me.com>
2020-12-09 10:23:18 +1100

    Add System.Text.Json processor

---
+++ src/DotnetSerializationCompressionBenchmark/Processors/SystemTextJsonProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using DotnetSerializationCompressionBenchmark.Contracts;
+
+namespace DotnetSerializationCompressionBenchmark.Processors
+{
+    public class SystemTextJsonProcessor : IProcessor
+    {
+        public byte[] Process<T>(T item)
+        {
+            return JsonSerializer.SerializeToUtf8Bytes(item);
+        }
+
+        public T Unprocess<T>(byte[] bytes)
+        {
+            return JsonSerializer.Deserialize<T>(bytes);
+        }
+    }
+}
