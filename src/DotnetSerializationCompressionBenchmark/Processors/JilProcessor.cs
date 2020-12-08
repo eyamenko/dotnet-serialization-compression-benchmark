92ca366af896e63c5b506f90779efeffadb0f5b2
Eugene Yamenko <yamenko@me.com>
2020-12-08 16:52:12 +1100

    Add Jil processor

---
+++ src/DotnetSerializationCompressionBenchmark/Processors/JilProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using DotnetSerializationCompressionBenchmark.Contracts;
+using Jil;
+
+namespace DotnetSerializationCompressionBenchmark.Processors
+{
+    public class JilProcessor : IProcessor
+    {
+        public byte[] Process<T>(T item)
+        {
+            return Encoding.UTF8.GetBytes(JSON.Serialize(item));
+        }
+
+        public T Unprocess<T>(byte[] bytes)
+        {
+            return JSON.Deserialize<T>(Encoding.UTF8.GetString(bytes));
+        }
+    }
+}
