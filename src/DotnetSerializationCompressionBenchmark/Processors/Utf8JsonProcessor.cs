2593c9dd7bbe100740dcf9b11c4773470c8ebf91
Eugene Yamenko <yamenko@me.com>
2020-12-09 10:40:59 +1100

    Add Utf8Json processor

---
+++ src/DotnetSerializationCompressionBenchmark/Processors/Utf8JsonProcessor.cs
@@ -0,0 +1,18 @@
+﻿using DotnetSerializationCompressionBenchmark.Contracts;
+using Utf8Json;
+
+namespace DotnetSerializationCompressionBenchmark.Processors
+{
+    public class Utf8JsonProcessor : IProcessor
+    {
+        public byte[] Process<T>(T item)
+        {
+            return JsonSerializer.Serialize(item);
+        }
+
+        public T Unprocess<T>(byte[] bytes)
+        {
+            return JsonSerializer.Deserialize<T>(bytes);
+        }
+    }
+}
