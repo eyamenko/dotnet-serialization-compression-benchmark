7bcc8d2137b70fb0963f207aa0d449830f7ed6d0
Eugene Yamenko <yamenko@me.com>
2020-12-08 16:41:39 +1100

    Add BinaryFormatter processor

---
+++ src/DotnetSerializationCompressionBenchmark/Processors/BinaryFormatterProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using DotnetSerializationCompressionBenchmark.Contracts;
+
+namespace DotnetSerializationCompressionBenchmark.Processors
+{
+    public class BinaryFormatterProcessor : IProcessor
+    {
+        private readonly BinaryFormatter _binaryFormatter;
+
+        public BinaryFormatterProcessor()
+        {
+            _binaryFormatter = new BinaryFormatter();
+        }
+
+        public byte[] Process<T>(T item)
+        {
+            using var ms = new MemoryStream();
+
+            _binaryFormatter.Serialize(ms, item);
+
+            return ms.ToArray();
+        }
+
+        public T Unprocess<T>(byte[] bytes)
+        {
+            using var ms = new MemoryStream(bytes);
+
+            return (T)_binaryFormatter.Deserialize(ms);
+        }
+    }
+}
