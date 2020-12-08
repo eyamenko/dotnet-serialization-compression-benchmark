c6fac4ca6b2897d9969be1f66ddb1ab3dc2f66b8
Eugene Yamenko <yamenko@me.com>
2020-12-08 16:12:59 +1100

    Add processor interface

---
+++ src/DotnetSerializationCompressionBenchmark/Contracts/IProcessor.cs
@@ -0,0 +1,8 @@
+﻿namespace DotnetSerializationCompressionBenchmark.Contracts
+{
+    public interface IProcessor
+    {
+        byte[] Process<T>(T item);
+        T Unprocess<T>(byte[] bytes);
+    }
+}
