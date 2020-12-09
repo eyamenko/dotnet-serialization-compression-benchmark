76f549ed436acd1829468aff795f663f01b2aa79
Eugene Yamenko <yamenko@me.com>
2020-12-09 14:14:37 +1100

    Allow to pass options to MessagePack processor

--- src/DotnetSerializationCompressionBenchmark/Processors/MessagePackProcessor.cs
+++ src/DotnetSerializationCompressionBenchmark/Processors/MessagePackProcessor.cs
@@ -5,9 +5,16 @@ namespace DotnetSerializationCompressionBenchmark.Processors
 {
     public class MessagePackProcessor : IProcessor
     {
+        private readonly MessagePackSerializerOptions _options;
+
+        public MessagePackProcessor(MessagePackSerializerOptions options = null)
+        {
+            _options = options;
+        }
+
         public byte[] Process<T>(T item)
         {
-            return MessagePackSerializer.Serialize(item);
+            return MessagePackSerializer.Serialize(item, _options);
         }
 
         public T Unprocess<T>(byte[] bytes)
